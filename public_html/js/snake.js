606d886f1d8750accad402963224ccd3797e79f4
BrandonHerrartte <almostbrandon03@gmail.com>
2014-10-03 14:57:01 -0700

    COMMIT BRUH

---
+++ public_html/js/snake.js
@@ -0,0 +1,72 @@
+var snake;
+var snakeLength;
+var snakeSize;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+
+gameInitialize();
+snakeInitialize();
+gameDraw();
+snakeDraw();
+
+
+function gameInitialize() {
+  var canvas = document.getElementById("game-screen");
+  context = canvas.getContext("2d");
+  
+  screenWidth = window.innerWidth;
+  screenHeight = window.innerHeight;
+  
+  canvas.width = screenWidth;
+  canvas.height = screenHeight;
+  }
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(255, 162, 0)";
+    context.fillRect(0, 0, screenWidth, screenHeight);  
+}
+
+function snakeInitialize(){
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 50;
+    
+    for(var index = snakeLength - 1; index >= 0; index--) {
+        snake.push({
+           x: index,
+           y: 0
+            
+        });
+        
+        
+    }   
+}
+
+function snakeDraw( ){
+    for(var index = 0; index < snake.length; index++){
+    context.fillStyle = "black";
+    context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate(){
+    var snakeHeadX = snake[5].x;
+    var snakeHeadY = snake[5].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.Y = snakeHeadY;
+    snake.unshift(snakeTail);
+    
+    
+    
+}
